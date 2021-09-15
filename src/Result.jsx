2c02f075c3d2baa609272f4a3893153e84fc9f7e
Alpha <alphatero@icloud.com>
2021-09-15 10:00:27 +0800

    fixed toast issue

--- src/Result.jsx
+++ src/Result.jsx
@@ -1,30 +1,48 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useLocation } from 'react-router';
 import { useHistory } from 'react-router-dom';
 import { QRCode } from 'react-qr-svg';
-import Toast from './components/Toast';
+import clsx from 'clsx';
+import { useToast } from './components/Toast';
+
+/* global ClipboardItem */
+function copyToClipboard(text) {
+  const type = 'text/plain';
+  const blob = new Blob([text], { type });
+  const data = [new ClipboardItem({ [type]: blob })];
+
+  return navigator.clipboard.write(data);
+}
 
 function Result() {
   const location = useLocation();
   const history = useHistory();
   const shortURL = `${window.location.origin}/${location.state.data}`;
+  const setToast = useToast();
+  const myInput = useRef(null);
 
-  let myInput = null;
-  const copyToClipboard = () => {
-    myInput.select();
-    document.execCommand('copy');
-    // eslint-disable-next-line no-alert
-    alert(`Copied miuURL : ${myInput.value}`);
+  const onClick = () => {
+    if (!myInput.current) return;
+    myInput.current.select();
+    copyToClipboard(shortURL);
+    setToast('Copy To Clipboard');
   };
 
   return (
     <div>
-      <Toast />
-      <main className='px-6 md:px-20 grid lg:grid-cols-2 place-content-center gap-10 md:gap-20 max-w-screen-xl mx-auto'>
-        <section className='text-blue-500 my-auto text-center lg:text-left space-y-8 lg:space-y-8'>
-          <h1 className='text-2xl lg:text-4xl font-bold'>
-            Best URL Shortening WebApp
-          </h1>
+      <main
+        className={clsx(
+          'px-6 md:px-20 grid  max-w-screen-xl mx-auto',
+          'lg:grid-cols-2 place-content-center gap-10 md:gap-20',
+        )}
+      >
+        <section
+          className={clsx(
+            ' my-auto text-center  space-y-8 lg:space-y-8',
+            'text-blue-500 lg:text-left',
+          )}
+        >
+          <h1 className='text-2xl lg:text-4xl font-bold'>Best URL Shortening WebApp</h1>
           <p className='text-base'> Easy Link Shortening</p>
         </section>
 
@@ -34,16 +52,10 @@ function Result() {
               readOnly
               className='border border-blue-400 w-80 px-3 py-2 rounded-lg text-gray-600 bg-white'
               value={shortURL}
-              ref={(ref) => {
-                myInput = ref;
-              }}
+              ref={myInput}
             />
 
-            <button
-              onClick={copyToClipboard}
-              className='ml-2 bg-white py-1.5 px-3 rounded-lg'
-              type='button'
-            >
+            <button type='button' onClick={onClick} className='ml-2 bg-white py-1.5 px-3 rounded-lg'>
               <i className='far fa-copy text-xl text-gray-700' />
             </button>
           </div>
@@ -57,8 +69,8 @@ function Result() {
             value={shortURL}
           />
           <button
-            onClick={() => history.push('/')}
             type='button'
+            onClick={() => history.push('/')}
             className='border-2 border-blue-400 text-blue-400 p-2 rounded-xl'
           >
             Shorten a new URL
