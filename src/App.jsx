195cdc5c3437794a93153f823488e2c08128a94f
yc999333 <yc99993333@gmail.com>
2021-09-16 15:14:16 +0800

    change url name and fix empty result issie

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,10 @@ import { ToastProvider } from './components/Toast';
 
 function App() {
   return (
-    <div className='min-h-screen '>
+    <div className='min-h-screen'>
       <header className='mb-5 bg-blue-400 px-6 shadow-md'>
         <h1 className='text-white mx-auto py-6 text-left md:pl-20 text-2.5xl lg:text-5xl font-bold max-w-screen-xl'>
-          <a href='/'>miuURL</a>
+          <a href='/'>μ-URL</a>
         </h1>
       </header>
 
