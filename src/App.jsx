3e6afd26fd2c3690c9813dc4ec50bd64524be931
Alpha <alphatero@icloud.com>
2021-10-12 16:29:32 +0800

    add notfound page

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,15 @@ import Footer from './Footer';
 import Result from './Result';
 import Redirect from './Redirect';
 import { ToastProvider } from './components/Toast';
-// import { Icon } from './components/Icon';
+import { Icon } from './components/Icon';
 
 function App() {
   return (
     <div className='min-h-screen flex flex-col justify-between'>
       <header className='mb-5 bg-blue-400 px-6 shadow-md'>
-        <h1 className='text-white mx-auto py-6 text-left md:pl-20 text-2.5xl lg:text-5xl font-bold max-w-screen-xl'>
+        <h1 className='text-white mx-auto py-6 text-left md:pl-20 text-2.5xl lg:text-5xl font-bold max-w-screen-xl flex justify-start'>
           <a href='/'>
-            {/* <Icon.Logo className='text-white h-10' /> */}
-            μRL
+            <Icon.Logo className='text-white h-10' />
           </a>
         </h1>
       </header>
--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,5 +1,6 @@
 import { ReactComponent as Success } from '../images/success.svg';
-// import { ReactComponent as Logo } from '../images/Urllogo.svg';
+import { ReactComponent as Logo } from '../images/Urllogo.svg';
+import { ReactComponent as Notfound } from '../images/404.svg';
 
-export const Icon = { Success };
+export const Icon = { Success, Logo, Notfound };
 export default Icon;
